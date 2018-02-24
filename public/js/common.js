4f164b90a222e0909e669df943cea51fdd0b54af
Alexander <kroser@i.ua>
2018-02-24 10:33:09 +0300

    upgrade

---
+++ public/js/common.js
@@ -0,0 +1,44 @@
+// bg-random
+$(function () {
+  var images = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg', 'bg5.jpg', 'bg6.jpg'];
+  $('.site-wrapper-home').css({
+    'background-image': 'url(public/img/bg/' + images[Math.floor(Math.random() * images.length)] + ')'
+  });
+});
+// bg-random end
+
+// wow
+$(function() {
+	new WOW().init();
+});
+// wow end
+// top
+$('.top').click(function() {
+  $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop() > $(window).height()) {
+    $('.top').addClass("active");
+  } else {
+    $('.top').removeClass("active");
+  };
+});
+// top end
+
+// sidebar
+$(function(document) {
+  var toggle = document.querySelector('.sidebar-toggle');
+  var sidebar = document.querySelector('#sidebar');
+  var checkbox = document.querySelector('#sidebar-checkbox');
+
+  document.addEventListener('click', function(e) {
+    var target = e.target;
+
+    if(!checkbox.checked ||
+       sidebar.contains(target) ||
+       (target === checkbox || target === toggle)) return;
+
+    checkbox.checked = false;
+  }, false);
+})(document);
+// sidebar end
